e02ed5c8d0bca14ae5b7ec373ef9108d538e958a
Husseinibrahim2 <husseinib23920@gmail.com>
2025-03-17 11:09:52 +0100

    Implement ProductController class

--- src/main/java/com/learn/FirstApp/controller/ProductController.java
+++ src/main/java/com/learn/FirstApp/controller/ProductController.java
@@ -1,4 +1,40 @@
 package com.learn.FirstApp.controller;
 
+import com.learn.FirstApp.model.Product;
+import com.learn.FirstApp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> findAll()
+    {
+        return productService.findAll();
+    }
+    @GetMapping("{id}")
+    public Product getProductById(Long id)
+    {
+        return productService.getProductById(id);
+    }
+    @DeleteMapping("{id}")
+    public void deleteProduct(Long id)
+    {
+        productService.deleteProduct(id);
+    }
+    @PutMapping
+    public void updateProduct(Product p)
+    {
+        productService.updateProduct(p);
+    }
+    @PostMapping
+    public void addProduct(Product product)
+    {
+        productService.addProduct(product);
+    }
 }
